e64792096dd05f18913a013aaf1d21448fe4fca3
sT00ne <wangshidong1115@gmail.com>
2016-06-27 22:39:03 +0800

    GetMovieName
    可以获取top250的片名

--- Top250.py
+++ Top250.py
@@ -1,27 +1,41 @@
 import re
 import urllib.request
 
-url = "https://movie.douban.com/top250"
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.116 Safari/537.36'}
-req = urllib.request.Request(url=url, headers=headers)
-page = urllib.request.urlopen(req)
-contentBytes = page.read()
-# contentBytes = contentBytes.decode('utf-8','ignore')
-# contentBytes = contentBytes.encode('utf-8')
-# contentBytes = contentBytes.decode('utf-8')
-# # print(contentBytes)
-# with open('e:/pp/webapp/www/a.txt', 'w', encoding='utf-8') as f:
-#      f.write(contentBytes)
-contentBytes = contentBytes.decode('utf-8')
-divre = r'<div class="hd">([\s\S]*?)</div>'
-divtext = re.findall(divre, contentBytes)
-for text in divtext:
-    spanre = r'<span class="title">([\s\S]*?)</span>'
-    spantext = re.findall(spanre, text)
 
-spantext = re.findall(r'<div class="hd">(.*)</div>', text)
-# images = re.findall(r'^https://[0-9a-zA-Z]*(png|gif|jpg)',str(contentBytes))
-# print(images)
-# for link,t in images:
-# 	urllib.request.urlretrieve("https://img3.doubanio.com/view/photo/thumb/public/p"+link+"."+t,"img/"+link+"."+t)
+def getmovie(i, url):
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_3) AppleWebKit/537.36 (KHTML, like Gecko) '
+                      'Chrome/48.0.2564.116 Safari/537.36'}
+    req = urllib.request.Request(url=url, headers=headers)
+    page = urllib.request.urlopen(req)
+    contentBytes = page.read()
+    contentBytes = contentBytes.decode('utf-8')
+    divre = r'<div class="hd">([\s\S]*?)</div>'
+    divtext = re.findall(divre, contentBytes)
+    movienames = {}
+    # i = 1
+    for text in divtext:
+        spanre = r'<span class="title">([\s\S]*?)</span>'
+        spantext = re.findall(spanre, text)
+        singlemoviename = ""
+        for name in spantext:
+            singlemoviename += name
+        singlemoviename = singlemoviename.replace("&nbsp;", "")
+        singlemoviename = singlemoviename.replace("&#39;", "\'")
+        movienames[i] = singlemoviename
+        i += 1
+    for (k, v) in movienames.items():
+        print("dict[%s]=" % k, v)
+    return i, movienames
+
+
+url = "https://movie.douban.com/top250"
+page = 0
+i = 1
+movienames = {}
+while page < 226:
+    pageurl = "?start=" + str(page) + "&filter="
+    movieurl = url + pageurl
+    i, movienamedict = getmovie(i, movieurl)
+    movienames.update(movienamedict)
+    page += 25

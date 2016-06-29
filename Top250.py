48a9ff66b2abb8fde866520277bfcddb458081d0
st <123>
2016-06-29 16:42:35 +0800

    upload top250.py

--- Top250.py
+++ Top250.py
@@ -1,18 +1,21 @@
 import re
 import urllib.request
+import requests
+from bs4 import BeautifulSoup
 
 
+# 使用自带的库
 def getmovie(i, url):
     headers = {
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_3) AppleWebKit/537.36 (KHTML, like Gecko) '
                       'Chrome/48.0.2564.116 Safari/537.36'}
     req = urllib.request.Request(url=url, headers=headers)
-    page = urllib.request.urlopen(req)
-    contentBytes = page.read()
-    contentBytes = contentBytes.decode('utf-8')
+    pagesource = urllib.request.urlopen(req)
+    pagecontent = pagesource.read()
+    pagecontent = pagecontent.decode('utf-8')
     divre = r'<div class="hd">([\s\S]*?)</div>'
-    divtext = re.findall(divre, contentBytes)
-    movienames = {}
+    divtext = re.findall(divre, pagecontent)
+    movienamesdict = {}
     # i = 1
     for text in divtext:
         spanre = r'<span class="title">([\s\S]*?)</span>'
@@ -22,11 +25,26 @@ def getmovie(i, url):
             singlemoviename += name
         singlemoviename = singlemoviename.replace("&nbsp;", "")
         singlemoviename = singlemoviename.replace("&#39;", "\'")
-        movienames[i] = singlemoviename
+        movienamesdict[i] = singlemoviename
         i += 1
-    for (k, v) in movienames.items():
-        print("dict[%s]=" % k, v)
-    return i, movienames
+    # for (k, v) in movienamesdict.items():
+    #     print("dict[%s]=" % k, v)
+    return i, movienamesdict
+
+
+# 使用Requests以及BeautifulSoup库
+def getmovie_requests(i, url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, "html.parser")
+    soup.div['class'] = "item"
+    moviedict = {}
+    for x in soup.findAll(class_="hd"):
+        name = ""
+        for y in x.findAll(class_="title"):
+            name += y.text
+        moviedict[i] = name
+        i += 1
+    return i, moviedict
 
 
 url = "https://movie.douban.com/top250"
@@ -36,6 +54,8 @@ movienames = {}
 while page < 226:
     pageurl = "?start=" + str(page) + "&filter="
     movieurl = url + pageurl
-    i, movienamedict = getmovie(i, movieurl)
+    i, movienamedict = getmovie_requests(i, movieurl)
     movienames.update(movienamedict)
     page += 25
+for (k, v) in movienames.items():
+    print("dict[%s]=" % k, v)
